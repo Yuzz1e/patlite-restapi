aab57370b6a4cba020be663a15c7924b37006bdd
Tamagoham <gleamfever797@hotmail.com>
2025-08-12 20:19:58 +0900

    :tada: add blinking mode

--- main.go
+++ main.go
@@ -1,55 +1,80 @@
 package main
 
 import (
-  "fmt"
-  "net"
-  "net/http"
-  "log"
-  "encoding/json"
-  "io"
-  "time"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"time"
 )
 
 type Alert_Request struct {
 	Status string `json:"status"`
 	Alerts []struct {
-		Status string
-		Labels map[string]string
-	}
+		Status string            `json:"status"`
+		Labels map[string]string `json:"labels"`
+	} `json:"alerts"`
 }
 
 func alert_webhook(w http.ResponseWriter, r *http.Request) {
-    var req Alert_Request
-    j, _ := io.ReadAll(r.Body)
-    json.Unmarshal(j, &req)
-    fmt.Printf("%v\n", req)
-    severity := req.Alerts[0].Labels["severity"]
-    switch severity {
-      case "critical":
-          ChangePatlite(1,0,0,1) // red & beep
-      case "warning":
-          ChangePatlite(0,1,0,1) // yellow & beep
-      default:
-          fmt.Println("default")
-    }
+	var req Alert_Request
+	j, _ := io.ReadAll(r.Body)
+	json.Unmarshal(j, &req)
+	fmt.Printf("%v\n", req)
 
-    go func(){
-      time.Sleep(1000 * time.Millisecond)
-      ChangePatlite(0,0,0,0)
-    }()
+	mode := [7]int{0, 0, 0, 0, 1, 0, 0}
+	duration := 10 * time.Second
+
+	if len(req.Alerts) > 0 {
+		alert := req.Alerts[0]
+		severity := alert.Labels["severity"]
+		beepMode := alert.Labels["beep"]
+
+		// Set blinking and buzzer mode
+		switch severity {
+		case "critical":
+			mode = [7]int{0, 1, 0, 0, 0, 0, 0} // red
+		case "warning":
+			mode = [7]int{1, 0, 0, 0, 0, 0, 0} // yellow
+		}
+
+		// Buzzer
+		switch beepMode {
+		case "long":
+			mode[2] = 1 // buzz long
+		case "short":
+			mode[3] = 1 // buzz short
+		}
+	}
+
+	SetPatliteMode(mode)
+
+	go func() {
+		time.Sleep(duration)
+		SetPatliteMode([7]int{0, 0, 0, 0, 1, 0, 0})
+	}()
 }
 
-func main(){
-  ChangePatlite(0,0,0,0)
+func main() {
+	SetPatliteMode([7]int{0, 0, 0, 0, 1, 0, 0})
 
-  http.HandleFunc("/alert_webhook", alert_webhook)
-  fmt.Println("Listening 0.0.0.0:8085")
-  log.Fatal(http.ListenAndServe(":8085", nil))
+	http.HandleFunc("/alert_webhook", alert_webhook)
+	fmt.Println("Listening 0.0.0.0:8085")
+	log.Fatal(http.ListenAndServe(":8085", nil))
 }
 
-func ChangePatlite(red int, yellow int, green int, beep int){
-  data := (beep & 0x1) << 3 | (green & 0x1) << 2 | (yellow & 0x1) << 1 | (red & 0x1)
-  conn, _ := net.Dial("udp", "172.16.254.240:10000")
-  fmt.Fprintf(conn, "%c%c", 0x57, data)
-  //fmt.Printf("%c%x", 0x57, data)
+func SetPatliteMode(mode [7]int) {
+	data := 0
+	for i := 0; i < 7; i++ {
+		data |= (mode[i] & 0x1) << (6 - i)
+	}
+	conn, err := net.Dial("udp", "172.16.254.240:10000")
+	if err != nil {
+		log.Println("UDP send error:", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Fprintf(conn, "%c%c", 0x57, data)
 }
